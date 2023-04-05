72413a256f7c93b08b4073150eef92c1c82e1db6
sujankumar215 <sujankumar215@gmail.com>
2023-04-05 09:08:43 +0530

    flaks moduler code

---
+++ modules/cricket/route.py
@@ -0,0 +1,21 @@
+from cricket import app
+
+@app.route("/")
+def home():
+    
+    return '<h1>Hello Kunal Duran</h1>'
+
+
+@app.route("/batsman")
+def batsman():
+    return '<h1>I am a Batsman</h1>'
+
+
+@app.route("/bowler")
+def bowler():
+    return '''<h1>Leg Spinner</h1>
+    <ul>
+        <li>Economy: 5.33</li>
+        <li>Best: 5 for 31 in 10 overs</li>
+    </ul>
+    '''
